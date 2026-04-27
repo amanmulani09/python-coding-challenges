00babf21507aa1a9bfabc1ebadbd1887c37846be
amanmulani09 <mulaniaman0504@gmail.com>
2026-04-27 09:36:35 +0530

    feat: time & space complexity

---
+++ dsa-handbook/time-space-complexity.py
@@ -0,0 +1,35 @@
+"""
+things to calculate the time complexity 
+
+1. always consider wrost case scenario
+2. avoid constant values 
+3. avoid lower values 
+
+different types of time complexity
+
+1. Big O -> wrost case 
+2. theta(0) -> average case
+3. Omeaga -> best case 
+
+always focus on Big O
+
+
+eg. 1
+
+for i in range(0,n+1):
+    for j in range(0, n+i)
+
+n * n = n2 = o(n2)
+
+
+eg. 2 
+
+space complexity
+
+2 types of memory space 
+
+1. auxiluery  space -> the extra space used to solve the problem 
+2. input space -> the space used to store the input 
+
+
+"""
