72a9125d36084d935d2a4f86a64558ada9176124
amanmulani09 <mulaniaman0504@gmail.com>
2026-08-18 10:03:15 +0530

    feat(core): string

---
+++ random/string.py
@@ -0,0 +1,12 @@
+s = "Hello, World!"
+
+# retrieve all chars 
+
+# print(s[:])
+
+## get all chars before/after a position(index)
+
+
+print(s[7:]) # prints chars after 7 index ie. 7:rest..
+
+print(s[:5]) # prints chars till 5 index ie. ...rest:5
